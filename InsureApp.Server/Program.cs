fe0919c81a421ac94bccdb8acd9489dd31a3a20f
Dawott <dawid.tott@gmail.com>
2025-01-25 01:06:01 +0100

    Additional document controller

--- InsureApp.Server/Program.cs
+++ InsureApp.Server/Program.cs
@@ -1,4 +1,5 @@
 using InsureApp.Server.Model;
+using InsureApp.Server.Services;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -16,7 +17,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IFileService, FileService>();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
